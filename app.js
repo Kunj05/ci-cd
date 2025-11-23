8a7ac4cf2ba6902ae47e423a06aacdc8c02fd24f
Kunj05 <kunjchandak05@gmail.com>
2025-11-24 04:28:47 +0530

    new

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js app running on ECS!');
+    res.send('Hello from Node.js app running ECS!');
 });
 
 app.listen(PORT, () => {
